1ea15838fa15e823a953a9e5b45bc664c662e512
Thao <quynhanhthao.161@gmail.com>
2025-09-29 14:39:07 +1000

    add logger singleton design pattern

---
+++ backend/patterns/LoggerSingleton.js
@@ -0,0 +1,26 @@
+class Logger {
+    constructor() {
+        this.logs = [];
+    }
+    
+    get count_logs() {
+        return this.logs.length;
+    }
+
+    info(message) {
+        const timestamp = new Date().toISOString();
+        this.logs.push({ message, timestamp });
+        console.log(`Info: ${timestamp} - ${message}`);
+    }
+    
+    error(message) {
+        const timestamp = new Date().toISOString();
+        this.logs.push( {message, timestamp });
+        console.log(`Error: ${timestamp} - ${message}`);
+    }
+}
+
+const Logger = require('../patterns/LoggerSingleton');
+const logger = new Logger();
+
+module.exports = { Logger, logger };
